0832afcc5ca7cb299d4e8136c6f31c10d51d8cbd
Julian <julian.moik@googlemail.com>
2025-03-06 23:17:23 +0100

    default to dark mode

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -13,7 +13,7 @@ interface ThemeContextType {
 const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 
 export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-    const [theme, setTheme] = useState<Theme>("light");
+    const [theme, setTheme] = useState<Theme>("dark");
 
     useEffect(() => {
         loadTheme();
